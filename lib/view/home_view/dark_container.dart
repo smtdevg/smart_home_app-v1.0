9a15fcca09b39d5a58f3cddcd42a8ba34c2f196f
smtdevg <mitaco199@gmail.com>
2024-12-07 05:16:32 +0700

    new

--- lib/view/home_view/dark_container.dart
+++ lib/view/home_view/dark_container.dart
@@ -22,21 +22,25 @@ class DarkContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Tính toán tỷ lệ dựa trên kích thước màn hình
+    final screenWidth = MediaQuery.of(context).size.width;
+    final scaleFactor = screenWidth / 650; // 400 là kích thước tham chiếu mặc định
+
     return InkWell(
       onTap: onTap ?? () {},
       child: Container(
-        width: getProportionateScreenWidth(140),
-        height: getProportionateScreenHeight(140),
+        width: 280 * scaleFactor, // Scale theo tỷ lệ
+        height: 140 * scaleFactor, // Scale theo tỷ lệ
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(20 * scaleFactor),
           color: itsOn
               ? const Color.fromRGBO(0, 0, 0, 1)
               : const Color(0xffededed),
         ),
         child: Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: getProportionateScreenWidth(10),
-            vertical: getProportionateScreenHeight(6),
+            horizontal: 10 * scaleFactor,
+            vertical: 6 * scaleFactor,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,17 +50,18 @@ class DarkContainer extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-                    width: 50,
-                    height: 50,
+                    width: 50 * scaleFactor,
+                    height: 50 * scaleFactor,
                     decoration: BoxDecoration(
                       color: itsOn
                           ? const Color.fromRGBO(45, 45, 45, 1)
                           : const Color(0xffdadada),
-                      borderRadius:
-                      const BorderRadius.all(Radius.elliptical(45, 45)),
+                      borderRadius: BorderRadius.circular(45 * scaleFactor),
                     ),
                     child: SvgPicture.asset(
                       iconAsset,
+                      width: 24 * scaleFactor,
+                      height: 24 * scaleFactor,
                       color: itsOn ? Colors.amber : const Color(0xFF808080),
                     ),
                   ),
@@ -69,15 +74,16 @@ class DarkContainer extends StatelessWidget {
                     device,
                     textAlign: TextAlign.left,
                     style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                      fontSize: Theme.of(context).textTheme.displayMedium!.fontSize! * scaleFactor,
                       color: itsOn ? Colors.white : Colors.black,
                     ),
                   ),
                   Text(
                     deviceCount,
                     textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: Color.fromRGBO(166, 166, 166, 1),
-                      fontSize: 13,
+                    style: TextStyle(
+                      color: const Color.fromRGBO(166, 166, 166, 1),
+                      fontSize: 13 * scaleFactor,
                       letterSpacing: 0,
                       fontWeight: FontWeight.normal,
                       height: 1.6,
@@ -92,18 +98,19 @@ class DarkContainer extends StatelessWidget {
                     itsOn ? 'On' : 'Off',
                     textAlign: TextAlign.left,
                     style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                      fontSize: Theme.of(context).textTheme.displayMedium!.fontSize! * scaleFactor,
                       color: itsOn ? Colors.white : Colors.black,
                     ),
                   ),
-                  if (switchButton != null) // Chỉ hiển thị nút nếu có `switchButton`
+                  if (switchButton != null)
                     GestureDetector(
                       onTap: switchButton,
                       child: Container(
-                        width: 48,
-                        height: 25.6,
-                        padding: const EdgeInsets.all(2),
+                        width: 48 * scaleFactor,
+                        height: 25.6 * scaleFactor,
+                        padding: EdgeInsets.all(2 * scaleFactor),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(20 * scaleFactor),
                           color: itsOn ? Colors.black : const Color(0xffd6d6d6),
                           border: Border.all(
                             color: const Color.fromRGBO(255, 255, 255, 1),
@@ -114,11 +121,11 @@ class DarkContainer extends StatelessWidget {
                           children: [
                             itsOn ? const Spacer() : const SizedBox(),
                             Container(
-                              width: 20,
-                              height: 20,
+                              width: 20 * scaleFactor,
+                              height: 20 * scaleFactor,
                               decoration: BoxDecoration(
                                 color: Colors.white,
-                                borderRadius: BorderRadius.circular(50),
+                                borderRadius: BorderRadius.circular(50 * scaleFactor),
                               ),
                             ),
                           ],
